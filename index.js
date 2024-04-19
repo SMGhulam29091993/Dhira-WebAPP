a17e4cdb62e94d3830cacea0a862f579e2b9286a
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-04-19 23:14:19 +0530

    created the model of employee

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const PORT = 8000;
+const cors = require("cors");
 
 const app = express();
 
 const db = require("./config/mongoose.js");
+
+app.use(cors())
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
 app.use("/", require("./routes"))
 
